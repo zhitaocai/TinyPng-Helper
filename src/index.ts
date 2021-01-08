f6df216112d5d2ade316a326c99f3005004f2523
caizhitao <czt.saisam@gmail.com>
2021-01-08 09:47:06 +0800

    完成功能

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ export class TinyHelper {
     async start() {
         let taskConfig = new TaskConfig();
         await new TinyCompressTask().handle(taskConfig);
-        console.log("恭喜，处理成功！");
     }
 }
 
--- src/tasks/TinyCompressTask.ts
+++ src/tasks/TinyCompressTask.ts
@@ -9,6 +9,26 @@ import tinify = require("tinify");
 
 export class TinyCompressTask implements TaskInterface {
     /**
+     * 当前已经压缩的图片数量
+     */
+    private _curCompressedImageCount: number = 0;
+
+    /**
+     * 总共需要压缩的图片数量
+     */
+    private _totalCompressedImageCount: number = 0;
+
+    /**
+     * 所有图片压缩前的总大小（字节数）
+     */
+    private _srcSizeInBytes: number = 0;
+
+    /**
+     * 所有图片压缩后的总大小（字节数）
+     */
+    private _finalSizeInBytes: number = 0;
+
+    /**
      * 处理任务
      *
      * @param taskConfig 配置参数
@@ -18,28 +38,48 @@ export class TinyCompressTask implements TaskInterface {
         let imgs: ImageModel[] = [];
         this._collectImageFilePaths(taskConfig.imgDirPath, imgs);
 
-        // 计算图片大小
-        let srcSizeInBytes = 0;
+        // 初始化数据
+        this._curCompressedImageCount = 0;
+        this._totalCompressedImageCount = imgs.length;
+        this._finalSizeInBytes = 0;
+        this._srcSizeInBytes = 0;
         imgs.forEach((img) => {
-            srcSizeInBytes += FileUtil.getFileSize(img.filePath);
+            this._srcSizeInBytes += FileUtil.getFileSize(img.filePath);
         });
-        let reableSrcSize = FileUtil.toReadableFileSize(srcSizeInBytes);
-        console.log(`图片处理：找到 ${imgs.length} 张原始图片，总大小 ${reableSrcSize}`);
+        console.log(`找到 ${imgs.length} 张原始图片，总大小 ${FileUtil.toReadableFileSize(this._srcSizeInBytes)}`);
+        console.log("");
 
         // 执行加密（替换原文件）
         tinify.key = taskConfig.tinyKey;
-        let finalSizeInBytes = 0;
-        for (let img of imgs) {
-            let srcSize = FileUtil.getFileSize(img.filePath);
-            await tinify.fromFile(img.filePath).toFile(img.filePath);
-            let compressedSize = FileUtil.getFileSize(img.filePath);
-            finalSizeInBytes += compressedSize;
-            let compressRate = (srcSize - compressedSize) / srcSize;
-            console.log(`压缩图片 ${img.filePath} 大小 ${FileUtil.toReadableFileSize(srcSize)} -> ${FileUtil.toReadableFileSize(compressedSize)} 压缩了 ${compressRate.toFixed(1)}%`);
-        }
-        let reableFinalSize = FileUtil.toReadableFileSize(finalSizeInBytes);
-        let totalCompressRate = (srcSizeInBytes - finalSizeInBytes) / srcSizeInBytes;
-        console.log(`压缩所有图片完成，大小 ${reableSrcSize} -> ${reableFinalSize} 压缩了 ${totalCompressRate.toFixed(1)}%`);
+        await Promise.all(
+            imgs.map((img) => {
+                return this._compressImage(img.filePath);
+            })
+        );
+        let totalCompressRate = (this._srcSizeInBytes - this._finalSizeInBytes) / this._srcSizeInBytes;
+        console.log("");
+        console.log(
+            `压缩所有图片完成，大小变化 ${FileUtil.toReadableFileSize(this._srcSizeInBytes)} -> ${FileUtil.toReadableFileSize(this._finalSizeInBytes)} 压缩了 ${totalCompressRate.toFixed(1)}%`
+        );
+    }
+
+    /**
+     * 压缩单个图片
+     *
+     * @param filePath 图片路径
+     */
+    private async _compressImage(filePath: string) {
+        let srcSize = FileUtil.getFileSize(filePath);
+        await tinify.fromFile(filePath).toFile(filePath);
+        let compressedSize = FileUtil.getFileSize(filePath);
+        this._finalSizeInBytes += compressedSize;
+        this._curCompressedImageCount++;
+        let compressRate = (srcSize - compressedSize) / srcSize;
+        console.log(
+            `(${this._curCompressedImageCount}/${this._totalCompressedImageCount}) 压缩图片 ${filePath} 大小变化 ${FileUtil.toReadableFileSize(srcSize)} -> ${FileUtil.toReadableFileSize(
+                compressedSize
+            )} 压缩了 ${compressRate.toFixed(1)}%`
+        );
     }
 
     /**
